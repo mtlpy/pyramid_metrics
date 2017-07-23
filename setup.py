2d5a3c0883614401c588f4b6d446800e38ed026f
Pior Bastida <pior@pbastida.net>
2017-07-23 14:47:05 -0400

    Stop using d2to1 (unsupported)

--- setup.py
+++ setup.py
@@ -1,6 +1,46 @@
-import setuptools
+import os
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    setup_requires=['d2to1'],
-    d2to1=True,
+install_requires = [
+    'statsd >= 2.1.2, < 2.2',
+]
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pyramid_metrics',
+    version='0.3.1.dev0',
+    author='Pior Bastida',
+    author_email='pior@pbastida.net',
+    description='Performance metrics for Pyramid using StatsD',
+    license='BSD',
+    keywords='wsgi pylons pyramid statsd metric tween handler',
+    url='https://github.com/ludia/pyramid_metrics',
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+    ],
+    packages=find_packages(),
+    package_data={
+        'devup': ['files/*'],
+    },
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'de = devup.app:cli',
+        ],
+    },
 )
